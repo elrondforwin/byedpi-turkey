93b1c67a27cad4702542368c52b2588665ba4d6a
Oğuz <132291321+elrondforwin@users.noreply.github.com>
2025-10-02 21:09:17 +0300

    byedpi surumu guncellendi

--- make.sh
+++ make.sh
@@ -47,7 +47,7 @@ cmd_install () {
     printf "${C_BOLD}- Downloading and preparing the dependencies${C_RESET}\n"
     printf -- "- Server\n"
     curl -L --progress-bar -o "$tmp_dir/ciadpi.tar.gz" \
-        "https://github.com/hufrea/byedpi/releases/download/v0.17.2/byedpi-17.2-$target_arch.tar.gz"
+        "https://github.com/hufrea/byedpi/releases/download/v0.17.3/byedpi-17.3-$target_arch.tar.gz"
     cd "$tmp_dir"
     tar -zxf "ciadpi.tar.gz"
     find -type f -name "ciadpi-*" -exec mv -f {} $BIN/ciadpi \;
